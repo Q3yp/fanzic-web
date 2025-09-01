5932b142a0ec8eea8ae6caf0889ca99373a2853f
padyue <padyue@gmail.com>
2025-09-01 11:56:57 +0800

    add cloudflare deploy support

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const createNextIntlPlugin = require('next-intl/plugin');
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+};
 
 module.exports = withNextIntl(nextConfig);
