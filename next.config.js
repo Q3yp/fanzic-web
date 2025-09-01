f98f08f122fdc1d697a5245aad3eed93a52e15c3
padyue <padyue@gmail.com>
2025-09-01 12:28:44 +0800

    add cloudflare deploy support

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
-const { setupDevPlatform } = require('@cloudflare/next-on-pages/next-dev');
 const createNextIntlPlugin = require('next-intl/plugin');
 
 const withNextIntl = createNextIntlPlugin();
-
-if (process.env.NODE_ENV === 'development') {
-  (async () => {
-    await setupDevPlatform();
-  })();
-}
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
